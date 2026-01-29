797ca305c38e00f99d8667c6210bf953bedf4ada
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 06:12:06 +0530

    updated

---
+++ variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Mansoor  # No space should be there between value  and the equal.
+PERSON2=Ramesh  # person -means variable should be always in capital letters.
+
+echo " $PERSON1 :: Hi $PERSON2, How are you ?"
+echo " $PERSON2 :: Hi $PERSON1, I am good,what about you?"
+echo " $PERSON1:: Yes $PERSON2, I am also fine, it's going on as of now "
+echo "$PERSON2:: $PERSON1, How was the classes going on "
+echo " $PERSON1 :: ya $PERSON2, it's good as of now, need to learn more about it "
