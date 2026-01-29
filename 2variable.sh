259d5eb6fee322524e8236b9069b6671a43310ed
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 07:17:50 +0530

    updated

--- 2variable.sh
+++ 2variable.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Please  enter your user name::  country name::  state name::"
+echo "Please  enter your user name::"
 read "USER_NAME"
-read "COUNTRY_NAME"
-read "STATE_NAME "
+
 echo " user name is $USER_NAME"
-echo "country nameis $COUNTRY_NAME"
-echo " state name is $STATE_NAME"
