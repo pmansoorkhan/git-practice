65c29dcceb8ded6f78d1488449768291c8d9576d
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 06:22:47 +0530

    updated

--- variable.sh
+++ variable.sh
@@ -8,3 +8,21 @@ echo " $PERSON2 :: Hi $PERSON1, I am good,what about you?"
 echo " $PERSON1:: Yes $PERSON2, I am also fine, it's going on as of now "
 echo "$PERSON2:: $PERSON1, How was the classes going on "
 echo " $PERSON1 :: ya $PERSON2, it's good as of now, need to learn more about it "
+
+
+
+#!/bin/bash
+
+PERSON1=Sarah  # No space should be there between value  and the equal.
+PERSON2=Marak  # person -means variable should be always in capital letters.
+
+echo " $PERSON1: Hey, $PERSON2! Is that you? I haven’t seen you in ages! "
+echo "$PERSON2: $PERSON1! Wow, what a surprise. It really has been a long time. How have you been? "
+echo "$PERSON1: I’ve been great, thanks! Just busy with work and life in general. What about you? Still working at the tech firm?" 
+echo "$PERSON2: Actually, I changed jobs a few months ago. I’m at a startup now. It’s a lot more work, but I’m really enjoying the challenge. "
+echo "$PERSON1: That’s awesome! Good for you. You always wanted to be in a more creative environment. "
+echo "$PERSON2: Thanks! It’s been a good move. Anyway, what are you doing this weekend? We should grab a coffee and actually catch up properly." 
+echo "$PERSON1: I’d love to! I’m free on Saturday afternoon. Does that work for you? "
+echo "$PERSON2: Saturday is perfect. Let’s meet at that new café downtown around 3:00 PM." 
+echo "$PERSON1: Sounds like a plan. I’ll see you then! "
+echo "$PERSON2: See you Saturday. Take care!
