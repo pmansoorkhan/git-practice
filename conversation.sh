7dc7e5b6bea8cb266112a9b08ffb671938f304c1
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 05:48:59 +0530

    updated

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " Mansoor :: Hi Ramesh, How are you ?"
+echo " Ramesh :: Hi Mansoor, I am good,what about you?"
+echo " Mansoor:: Yes Ramesh, I am also fine, it's going on as of now "
+echo "Ramesh:: Mansoor, How was the classes going on "
+echo " Mansoor :: ya ramesh, it's good as of now, need to learn more about it "
