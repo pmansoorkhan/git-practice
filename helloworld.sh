7445f3af2a68498337ee0ca189dd492874eddaae
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 05:38:40 +0530

    updated

--- helloworld.sh
+++ helloworld.sh
@@ -4,3 +4,5 @@ echo "Hello World"
 # temporary mdification 
 echo " Hi, I am learning GIT in devsecops classes"
 echo " Hi mansoor khan , welcome to the GIT practice session "
+
+echo " Hi mansoor, You are selected as DevOps Engineer with pacakage of 30 PLPA "
