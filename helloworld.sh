e82392688628d366f161173f6772c11d7efa0d43
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-29 05:40:49 +0530

    updated

--- helloworld.sh
+++ helloworld.sh
@@ -6,3 +6,4 @@ echo " Hi, I am learning GIT in devsecops classes"
 echo " Hi mansoor khan , welcome to the GIT practice session "
 
 echo " Hi mansoor, You are selected as DevOps Engineer with pacakage of 30 PLPA "
+echo "Pushpa: The Rise tells the story of Pushpa Raj, an illegitimate laborer in Andhra Pradesh who rises from being a low-caste worker to the kingpin of the red sandalwood smuggling syndicate, navigating betrayal, romance with Srivalli, and fierce conflicts with police and rival gangs, driven by his ambition to overcome his despised status. The film follows his audacious climb in the illegal timber trade, using cunning and fearlessness to outsmart authorities and fellow smugglers, ultimately making powerful enemies.  "
