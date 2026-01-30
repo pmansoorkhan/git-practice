5ff641ae1ce5570eb67439badfc80264958aa8fe
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 19:23:15 +0530

    Mehran

---
+++ maths.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo " enter your first number: "
+    read num1
+
+echo " enter your second number: "
+    read num2
+
+echo " Sum:$(($num1+$num2))"
+echo " Difference :$(($num1 - $num2))"
+echo " Product :$(($num1 * $num2))"
+echo " Division:$(($num1 / $num2))"
+echo " Remainder: $(($num1 % $num2))"
+
+echo " The value is Sum:; Difference:; ,Product: ; Division: ; Reminder:"
+
+
+
+
+
+
+
+
