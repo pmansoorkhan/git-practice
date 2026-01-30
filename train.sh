1b6809056f335e3f56e1d56171701eb0cf6dcd8b
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 18:36:46 +0530

    Mehran

---
+++ train.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "======BOOKING TRAIN TICKET====="
+
+echo "Enter Passenger Name:"
+    read Name
+
+echo "Enter Age:"
+    read Age
+
+ echo "Enter Gender:"
+    read Gender 
+
+echo "Enter Starting Location: "
+    read source
+
+echo "Enter destination location:"
+    read destination
+
+echo "Date of Jounrey (DD-MM-YYYY):" 
+    read date
+
+echo "Enter Mobile Number:"
+    read contact
+
+echo "Enter Seat Number"
+    read Seat
+
+
+    echo ""
+
+    echo "======BOOKING TRAIN TICKET====="
+    echo "$NAME"
+    echo "$AGE"
+    echo "$Gender"    
+    echo "$Source"
+    echo "$Destination"
+    echo "$Date"
+    echo "$Contact"
+    echo "$Seat"
+    echo "✅ Ticket booked successfully!"
+    echo "Indian Railways wishes you a happy  and safe journey" 
