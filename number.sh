f2a3e8020f0f6658071bb75a538c72581626bf1a
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 12:30:32 +0530

    hi

--- number.sh
+++ number.sh
@@ -1,13 +1,9 @@
-
-
-
-
-
 #!/bin/bash
-num="enter the number"
+num=10
 
-if [$num > 5 ]; then
-   echo "Number is greater than 5"
+if [ $num -gt 5 ]; then
+  echo "Number is greater than 5"
 else
-   echo "Number is 5 or less"
+  echo "Number is 5 or less"
 fi
+
