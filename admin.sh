9beddf12f4ff5520734939d8e6485a4d9fbca995
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 19:07:02 +0530

    Mehran

---
+++ admin.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+echo "**************************Welcome to the future****************** "
+
+echo " enter your Name :"
+    read Name
+echo " enter your country:"
+    read country
+echo "enter your state:"
+    read  state
+echo "your are born in the year (DD/MM/YYYY): "
+    read born
+echo "present you are in location:"
+    read location
+echo ""
+
+
+
+echo "&&&&&&&&&&&&&&&&&&&*Welcome to the future*&&&&&&&&&&&&&&&&&&&& "
+echo "Name      :$Name"
+echo "country   :$country"
+echo "state     :$state"
+echo "born      :$born"
+echo "location  :$location
+echo "THIS IS 2300 FUTURE CITY OF EARTH"
+echo " I am the humanoid robot in this world"
+
