62d627d8a9efc76b6c9ab04042ce29dc92f60e09
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 11:44:35 +0530

    hi

--- months.sh
+++ months.sh
@@ -6,7 +6,6 @@ echo "${months}"
 
 
 #!/bin/bash
-months=$@
-$=="january, febuary, Mach, April, May, June, July, August, September, october, November,December"
-
-echo "{months[$@]}"
+month=="january, febuary, Mach, April, May, June, July, August, September, october, November,December"
+months=$
+echo "{$}"
