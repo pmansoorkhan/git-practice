70d3db637bd3da32529954adbc251fb5953e2b9c
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-01-30 21:34:05 +0530

    Mehran

--- variable5.sh
+++ variable5.sh
@@ -9,8 +9,9 @@ echo " please enter your password:"
     read -s password
 
 
-echo "please enter your  height: &  weight:"
-    read -s "height & Weight"
+echo "please enter your  height:"
+echo "please enter your  Weight:"
+    read -s height & Weight
 echo " $height and $weight "
 
 
